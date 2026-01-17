7c0d03e4a7690271721b6d82f56969ad9301bfc8
Jared Mijail <jramirezes@unal.edu.co>
2026-01-17 14:53:27 -0500

    secority change for dbus

--- cockpit.nix
+++ cockpit.nix
@@ -82,10 +82,29 @@ in
     extraGroups = [ "libvirtd" ]; # Necesita acceso al socket de libvirt
   };
 
+  # Política D-Bus explícita para nuestro usuario custom 'libvirtdbus'
+  # Separada en un paquete para que services.dbus.packages la cargue sin conflictos
+  libvirt-dbus-policy = pkgs.writeTextFile {
+    name = "org.libvirt.conf";
+    destination = "/share/dbus-1/system.d/org.libvirt.conf";
+    text = ''
+      <!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-BUS Bus Configuration 1.0//EN"
+       "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+      <busconfig>
+        <policy user="libvirtdbus">
+          <allow own="org.libvirt"/>
+          <allow send_destination="org.libvirt"/>
+          <allow receive_sender="org.libvirt"/>
+        </policy>
+        <policy context="default">
+          <allow send_destination="org.libvirt"/>
+        </policy>
+      </busconfig>
+    '';
+  };
+
   # Registrar el servicio DBus
-  services.dbus.packages = [ libvirt-dbus ]; # El paquete contiene la política, la usaremos
-  
-  # systemd.packages = [ libvirt-dbus ]; # Falló al encontrar la unidad, usaremos definición manual
+  services.dbus.packages = [ libvirt-dbus libvirt-dbus-policy ]; # Cargamos el binario Y la política manual
 
   # Definición manual del servicio para asegurar que existe y apunta al binario correcto
   systemd.services.libvirt-dbus = {
