d11bc7b041d292924baa56f524986a19443163da
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-17 23:51:41 +0000

    estesies

--- cockpit.nix
+++ cockpit.nix
@@ -1,14 +1,13 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Definimos el paquete manualmente usando el link de GitHub
+  # Definición manual desde GitHub
   cockpit-machines-manual = pkgs.stdenv.mkDerivation rec {
     pname = "cockpit-machines";
-    version = "331"; # Coincide con tu versión de Cockpit
+    version = "331";
 
     src = pkgs.fetchzip {
       url = "https://github.com/cockpit-project/cockpit-machines/releases/download/${version}/cockpit-machines-${version}.tar.xz";
-      # REEMPLAZA ESTA LÍNEA CON EL NUEVO HASH:
       sha256 = "sha256-x16eynAUoOqAw4FbbXus3+jus/HEnxFfXvyHkki5d2A="; 
     };
 
@@ -21,31 +20,46 @@ let
   };
 in
 {
-  # 1. Habilitar Cockpit y añadir el paquete manual
+  # 1. Habilitar Cockpit
   services.cockpit = {
     enable = true;
     port = 9090;
-    package = pkgs.cockpit.overrideAttrs (oldAttrs: {
-      passthru = (oldAttrs.passthru or {}) // {
+    
+    # CORRECCIÓN AQUÍ: Forzamos la integración del paquete manual
+    package = pkgs.cockpit.override {
+      packageOverrides = {
+        # Esto le dice a NixOS que incluya nuestro paquete manual en la ruta de Cockpit
         extraPackages = [ cockpit-machines-manual ];
       };
-    });
+    };
+
     settings = {
       WebService = {
-        # Asegúrate de que esto sea una sola línea sin saltos extraños
         Origins = "https://192.168.8.123:9090 http://192.168.8.123:9090 http://localhost:9090";
       };
     };
   };
 
-  # 2. Virtualización (Necesaria para que las máquinas funcionen)
-  virtualisation.libvirtd.enable = true;
-  
+  # 2. Virtualización completa
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf.enable = true;
+    };
+  };
+
   # 3. Paquetes de apoyo
+  # Añadimos virt-install porque cockpit-machines lo usa para crear las VMs
   environment.systemPackages = with pkgs; [
     cockpit-machines-manual
     virt-manager
+    virt-viewer
+    virt-install
     libvirt
+    bridge-utils
   ];
 
   # 4. Permisos para tu usuario
