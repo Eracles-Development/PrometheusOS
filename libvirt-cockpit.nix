c28de87f790da0ba35737eb6380aa58b16254326
carl0sArauj0 <caraujoro@unal.edu.co>
2026-01-17 14:56:40 -0500

    fix lib

--- libvirt-cockpit.nix
+++ libvirt-cockpit.nix
@@ -7,32 +7,27 @@
     port = 9090;
     settings = {
       WebService = {
-        # He ajustado las IPs para que coincidan con tu captura (.122) y el resto de tu red
         Origins = lib.mkForce "https://eracles1:9090 https://127.0.0.1:9090 https://192.168.8.121:9090 https://192.168.8.122:9090 https://192.168.8.123:9090";
         ProtocolHeader = "X-Forwarded-Proto";
       };
     };
   };
 
-  # 2. Configuración de Virtualización (Libvirt)
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      ovmf.enable = true;
-      swtpm.enable = true;
-    };
-  };
+  # 2. Configuración de Virtualización
+  virtualisation.libvirtd.enable = true;
 
-  # 3. Paquetes necesarios
-  environment.systemPackages = [
-    pkgs.cockpit-machines
-    pkgs.virt-manager
-    pkgs.libvirt
-    pkgs.packagekit
-  ];
+  # 3. Paquetes con "Red de Seguridad"
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    libvirt
+    packagekit
+  ] 
+  # Esta línea intenta instalar cockpit-machines solo si el sistema lo encuentra
+  ++ (if pkgs ? cockpit-machines then [ pkgs.cockpit-machines ] else [ ])
+  # Por si acaso ha cambiado de lugar a cockpitPackages
+  ++ (if pkgs ? cockpitPackages.machines then [ pkgs.cockpitPackages.machines ] else [ ]);
 
-  # 4. Solución al "Limited Access" y permisos de Virtualización
+  # 4. Permisos de administración
   security.polkit.enable = true;
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, user) {
