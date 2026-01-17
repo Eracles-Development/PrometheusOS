e4e6da4a49844f4558e663f3bdd47fb792c6c997
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-17 23:02:36 +0000

    ere

--- cockpit.nix
+++ cockpit.nix
@@ -5,51 +5,37 @@
   services.cockpit = {
     enable = true;
     port = 9090;
-    package = pkgs.cockpit.override {
-      packageOverrides = {
-        cockpit-machines = pkgs.cockpit-machines;
-      };
+    
+    # FORMA CORRECTA PARA NIXOS 24.11:
+    # Usamos symlinkJoin para "unir" el cockpit base con sus módulos
+    package = pkgs.symlinkJoin {
+      name = "cockpit-with-plugins";
+      paths = [
+        pkgs.cockpit        # El núcleo de Cockpit
+        pkgs.cockpit-machines # El módulo de VMs
+      ];
+      # Esto asegura que el binario principal sea el que se ejecute
+      postBuild = ''
+        mkdir -p $out/share/cockpit
+        ln -snf ${pkgs.cockpit}/share/cockpit/* $out/share/cockpit/
+        ln -snf ${pkgs.cockpit-machines}/share/cockpit/* $out/share/cockpit/
+      '';
     };
+
     settings = {
       WebService = {
-        # Permitir acceso desde la red local
-        Origins = lib.mkForce "https://192.168.8.123:9090 http://192.168.8.123:9090 localhost:9090
-        ";
+        # Corregido el string para evitar saltos de línea accidentales
+        Origins = "https://192.168.8.123:9090 http://192.168.8.123:9090 http://localhost:9090";
       };
     };
   };
 
-  # Abrir puerto en el firewall
-  networking.firewall.allowedTCPPorts = [ 9090 ];
-
-  # Virtualización (libvirt + QEMU/KVM)
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      runAsRoot = true;
-      package = pkgs.qemu_kvm;
-      swtpm.enable = true;
-      ovmf.enable = true;
-    };
-    # Configuración de los sockets de libvirt para que el usuario pueda conectar sin sudo
-    extraConfig = ''
-      unix_sock_group = "libvirtd"
-      unix_sock_rw_perms = "0770"
-    '';
-  };
-  
-  # Arranque automático de libvirtd
-  systemd.services.libvirtd.wantedBy = [ "multi-user.target" ];
-
-  # Necesario para que virt-manager guarde su configuración
-  programs.dconf.enable = true;
-  
-  # Permisos de usuario (Ajusta 'eracles' si tu usuario es diferente, o muévelo a configuration.nix si prefieres)
+  # ... resto de tu configuración de libvirtd igual ...
+  # Asegúrate de mantener la virtualización y los grupos de usuario
+  virtualisation.libvirtd.enable = true;
   users.users.eracles.extraGroups = [ "libvirtd" "kvm" ];
 
-  # Paquetes del sistema para virtualización y Cockpit
   environment.systemPackages = with pkgs; [
-    # cockpit-machines # Comentado temporalmente por error 'undefined variable'
     qemu
     libvirt
     virt-manager 
