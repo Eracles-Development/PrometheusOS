17253ab5de0d41298794b47be118f605d55ad191
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-17 23:05:34 +0000

    ee

--- cockpit.nix
+++ cockpit.nix
@@ -6,39 +6,41 @@
     enable = true;
     port = 9090;
     
-    # FORMA CORRECTA PARA NIXOS 24.11:
-    # Usamos symlinkJoin para "unir" el cockpit base con sus módulos
-    package = pkgs.symlinkJoin {
-      name = "cockpit-with-plugins";
-      paths = [
-        pkgs.cockpit        # El núcleo de Cockpit
-        pkgs.cockpit-machines # El módulo de VMs
-      ];
-      # Esto asegura que el binario principal sea el que se ejecute
-      postBuild = ''
-        mkdir -p $out/share/cockpit
-        ln -snf ${pkgs.cockpit}/share/cockpit/* $out/share/cockpit/
-        ln -snf ${pkgs.cockpit-machines}/share/cockpit/* $out/share/cockpit/
-      '';
-    };
+    # Esta es la forma más limpia y robusta para 24.11
+    # Combinamos el paquete base con el plugin de máquinas
+    package = pkgs.cockpit.overrideAttrs (oldAttrs: {
+      passthru = (oldAttrs.passthru or {}) // {
+        # Esto le dice a Cockpit dónde buscar módulos extra
+        extraPackages = [ pkgs.cockpit-machines ];
+      };
+    });
 
     settings = {
       WebService = {
-        # Corregido el string para evitar saltos de línea accidentales
         Origins = "https://192.168.8.123:9090 http://192.168.8.123:9090 http://localhost:9090";
       };
     };
   };
 
-  # ... resto de tu configuración de libvirtd igual ...
-  # Asegúrate de mantener la virtualización y los grupos de usuario
-  virtualisation.libvirtd.enable = true;
+  # Virtualización y Libvirtd
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      runAsRoot = true;
+      package = pkgs.qemu_kvm;
+      swtpm.enable = true;
+      ovmf.enable = true;
+    };
+  };
+  
+  # Aseguramos que el usuario tenga los permisos necesarios
   users.users.eracles.extraGroups = [ "libvirtd" "kvm" ];
 
+  # Añadimos los paquetes necesarios al sistema
   environment.systemPackages = with pkgs; [
+    cockpit-machines # Lo intentamos añadir aquí también para asegurar visibilidad
     qemu
     libvirt
     virt-manager 
-    xorg.xauth
   ];
 }
