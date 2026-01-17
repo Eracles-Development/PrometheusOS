5dbf46602ee1af8304333ddbe7d2a4f962201191
Jared Mijail <jramirezes@unal.edu.co>
2026-01-16 21:23:52 -0500

    virtual manager replece cockpit

--- configuration.nix
+++ configuration.nix
@@ -87,7 +87,12 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-   services.openssh.enable = true;
+   services.openssh = {
+     enable = true;
+     settings = {
+       X11Forwarding = true;
+     };
+   };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- libvirt-cockpit.nix
+++ libvirt-cockpit.nix
@@ -10,48 +10,16 @@
   # (Opcional pero recomendado) soporte para TPM en VMs, etc.
   # virtualisation.libvirtd.qemu.swtpm.enable = true;
 
-  ###### Cockpit (interfaz web) ######
-  services.cockpit.enable = true;
-
-  # Abre el puerto de Cockpit (9090) en el firewall automáticamente
-  services.cockpit.openFirewall = true;
-
-  # Aseguramos que openssl esté disponible para generar certificados
+  ###### Paquetes (Libvirt + Herramientas GUI) ######
   environment.systemPackages = with pkgs; [
-    cockpit
-    openssl
     qemu
     libvirt
-    virt-manager
+    virt-manager  # GUI necesaria para X11 Forwarding
+    xauth         # Requerido para X11 Forwarding
   ];
-
-  # Permitir acceso HTTP sin restricciones para evitar problemas de certificados
-  services.cockpit.settings = {
-    WebService = {
-      AllowUnencrypted = true;
-    };
-  };
-
-  # LIMPIEZA: Eliminamos el certificado manual que causaba errores.
-  # Cockpit generará uno temporal en memoria si es necesario, pero usaremos HTTP.
-  systemd.services.clean-cockpit-cert = {
-    description = "Remove problematic custom certificate";
-    wantedBy = [ "multi-user.target" ];
-    before = [ "cockpit.service" ];
-    script = ''
-      rm -f /etc/cockpit/ws-certs.d/01-self-signed.cert
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-    };
-  };
 
   ###### Permisos de usuario ######
   users.users.eracles.extraGroups = [ "libvirtd" "kvm" ];
-
-  ###### Red/firewall (si no usas openFirewall) ######
-  # Si prefieres controlar el firewall a mano, comenta openFirewall y usa esto:
-  # networking.firewall.allowedTCPPorts = [ 9090 ];
 
   ###### Arranque automático de libvirtd ######
   # Normalmente NixOS lo hace al habilitar libvirtd, pero lo dejamos explícito:
