38c70d60c8e8f4589646d6c136c5773c691fa60f
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-17 17:25:52 +0000

    vm&pcp

--- libvirt-cockpit.nix
+++ libvirt-cockpit.nix
@@ -1,52 +1,40 @@
 { config, pkgs, lib, ... }:
 
 {
-  ###### Virtualización (libvirt + QEMU/KVM) ######
-  virtualisation.libvirtd.enable = true;
-
-  # Suele evitar problemas de permisos/compatibilidad con UIs
-  virtualisation.libvirtd.qemu.runAsRoot = true;
-
-  # (Opcional pero recomendado) soporte para TPM en VMs, etc.
-  # virtualisation.libvirtd.qemu.swtpm.enable = true;
-
-  ###### Cockpit (interfaz web) ######
-  services.cockpit.enable = true;
-
-  # Abre el puerto de Cockpit (9090) en el firewall automáticamente
-  services.cockpit.openFirewall = true;
-
-  # Aseguramos que openssl esté disponible para generar certificados
-  environment.systemPackages = with pkgs; [
-    cockpit
-    openssl
-    qemu
-    libvirt
-    virt-manager
-  ];
-
-  # Limpiar certificados corruptos antes de que Cockpit inicie
-  systemd.services.cockpit-cleanup-certs = {
-    description = "Clean corrupted Cockpit certificates";
-    before = [ "cockpit.service" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
+  # 1. Configuración de Cockpit
+  services.cockpit = {
+    enable = true;
+    port = 9090;
+    settings = {
+      WebService = {
+        # Permitimos la IP y el nombre de host para evitar el rechazo de conexión
+        # Si quieres ser menos estricto mientras pruebas, puedes comentar la línea de Origins
+        Origins = lib.mkForce "https://192.168.8.122:9090 https://eracles1:9090
+        https://192.168.8.122:9090 http://192.168.8.122:9090
+        https://192.168.8.123:9090 http://192.168.8.123:9090";
+        ProtocolHeader = "X-Forwarded-Proto";
+      };
     };
-    script = ''
-      rm -rf /etc/cockpit/ws-certs.d/*
-      mkdir -p /etc/cockpit/ws-certs.d
-    '';
   };
 
-  ###### Permisos de usuario ######
-  users.users.eracles.extraGroups = [ "libvirtd" "kvm" ];
+  # 2. Configuración de Virtualización (Libvirt)
+  virtualisation.libvirtd.enable = true;
+  
+  # 3. Paquetes necesarios
+  environment.systemPackages = with pkgs; [
+    cockpit
+    cockpit-machines
+    cockpit-pcp
+    virt-manager
+    libvirt
+    openssl
+  ];
 
-  ###### Red/firewall (si no usas openFirewall) ######
-  # Si prefieres controlar el firewall a mano, comenta openFirewall y usa esto:
-  # networking.firewall.allowedTCPPorts = [ 9090 ];
+  # 4. Permisos y Firewall
+  security.polkit.enable = true;
+  networking.firewall.allowedTCPPorts = [ 9090 ];
 
-  ###### Arranque automático de libvirtd ######
-  systemd.services.libvirtd.wantedBy = [ "multi-user.target" ];
+  # 5. PCP (Performance Co-Pilot) para métricas históricas
+  services.pmcd.enable = true;
+  services.pmlogger.enable = true;
 }
