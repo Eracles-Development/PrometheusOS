85f8c9904edecbd3fbdc4d8ae4c66177c3edad86
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-18 02:28:54 +0000

    hhhiiii

--- cockpit.nix
+++ cockpit.nix
@@ -6,8 +6,7 @@ let
     version = "328";
     src = pkgs.fetchzip {
       url = "https://github.com/cockpit-project/cockpit-machines/releases/download/${version}/cockpit-machines-${version}.tar.xz";
-      # HASH CORREGIDO SEGÚN TU ERROR:
-      sha256 = "sha256-HlmvnWoVnN3Ju9EomlcM6j3a0MoZzqZ9OXqQxkUT4qs="; 
+      sha256 = "sha256-HlmvnWoVnN3Ju9EomlcM6j3a0MoZzqZ9OXqQxkUT4qs=";
     };
     nativeBuildInputs = [ pkgs.gettext pkgs.findutils pkgs.gnused ];
 
@@ -15,32 +14,25 @@ let
       mkdir -p $out/share/cockpit/machines
       SOURCE_DIR=$(find . -name manifest.json -exec dirname {} \; | head -n 1)
       cp -r $SOURCE_DIR/* $out/share/cockpit/machines/
-      # Limpiamos condiciones incompatibles con NixOS
+      # Cirugía de manifest para compatibilidad total
       sed -i '/"conditions": \[/,/ \],/d' $out/share/cockpit/machines/manifest.json
     '';
   };
 in
 {
-  # 1. Cockpit y su visibilidad
+  # 1. Cockpit: Inyección de Paquetes Extra
   services.cockpit = {
     enable = true;
     port = 9090;
+    # Esto es vital: añadimos el plugin a la lista de paquetes que Cockpit escanea nativamente
     package = pkgs.cockpit.overrideAttrs (oldAttrs: {
-      passthru = (oldAttrs.passthru or {}) // {
-        extraPackages = [ cockpit-machines-manual ];
-      };
-      # Unimos el plugin al paquete principal
       postInstall = (oldAttrs.postInstall or "") + ''
         ln -s ${cockpit-machines-manual}/share/cockpit/machines $out/share/cockpit/machines
       '';
     });
-    settings.WebService.Origins = "https://192.168.8.123:9090 http://192.168.8.123:9090";
   };
 
-  # Variable crítica para que el bridge encuentre los archivos
-  systemd.services.cockpit.environment.COCKPIT_DATA_DIR = "/run/current-system/sw/share/cockpit";
-
-  # 2. Virtualización Libvirtd (Configuración Robusta)
+  # 2. Virtualización: Eliminación de Sockets Inactivos
   virtualisation.libvirtd = {
     enable = true;
     onShutdown = "shutdown";
@@ -50,7 +42,7 @@ in
       swtpm.enable = true;
       ovmf.enable = true;
     };
-    # Permisos de socket para evitar el error de descriptor/permisos
+    # Configuración de comunicación IPC
     extraConfig = ''
       unix_sock_group = "libvirtd"
       unix_sock_rw_perms = "0770"
@@ -58,35 +50,41 @@ in
     '';
   };
 
-  # 3. Forzar persistencia y timeout 0
+  # 3. Systemd: Forzar Persistencia de Estado
   systemd.services.libvirtd = {
+    path = [ pkgs.libvirt pkgs.qemu_kvm ];
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
       ExecStart = lib.mkForce [ "" "${pkgs.libvirt}/sbin/libvirtd --timeout 0" ];
-      Restart = lib.mkForce "always";
-      RestartSec = "5s";
+      Restart = "always";
+      RestartSec = "2s";
     };
   };
 
-  # 4. Reglas de Polkit para autorizar a Cockpit
+  # 4. Seguridad: Polkit (La llave maestra)
+  # Esto soluciona el "Permission Denied" y el fallo de descriptor
+  security.polkit.enable = true;
   security.polkit.extraConfig = ''
     polkit.addRule(function(action, subject) {
-      if (action.id == "org.libvirt.unix.manage" && subject.isInGroup("libvirtd")) {
+      if ((action.id == "org.libvirt.unix.manage" || 
+           action.id.indexOf("org.cockpit-project.cockpit-bridge") !== -1) &&
+          subject.isInGroup("libvirtd")) {
         return polkit.Result.YES;
       }
     });
   '';
 
-  # 5. Paquetes y Usuario
+  # 5. Entorno de Sistema
   environment.systemPackages = with pkgs; [
     cockpit-machines-manual
-    virt-manager
     libvirt
+    qemu_kvm
     bridge-utils
   ];
 
+  # Aseguramos que el usuario tenga herencia de grupos correcta
   users.users.eracles = {
     isNormalUser = true;
-    extraGroups = [ "libvirtd" "kvm" "wheel" "libvirt" ];
+    extraGroups = [ "libvirtd" "kvm" "wheel" "libvirt" "qemu-libvirtd" ];
   };
 }
