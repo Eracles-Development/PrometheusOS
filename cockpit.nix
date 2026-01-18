8b56f26629d38cea2cef0ab71bb453f2b7988b98
Luis Felipe Ortega Castellanos <luisortcas@unisabana.edu.co>
2026-01-18 01:49:59 +0000

    esteeeeessssshpta

--- cockpit.nix
+++ cockpit.nix
@@ -15,17 +15,16 @@ let
       SOURCE_DIR=$(find . -name manifest.json -exec dirname {} \; | head -n 1)
       cp -r $SOURCE_DIR/* $out/share/cockpit/machines/
       
-      # --- LA CORRECCIÓN CLAVE ---
-      # Borramos la sección de "conditions" que busca rutas de Ubuntu/RedHat que en NixOS no existen
+      # Eliminamos las condiciones de rutas fijas que no existen en NixOS
       sed -i '/"conditions": \[/,/ \],/d' $out/share/cockpit/machines/manifest.json
     '';
   };
 in
 {
+  # 1. Servicio Cockpit
   services.cockpit = {
     enable = true;
     port = 9090;
-    # Inyectamos el paquete modificado directamente en Cockpit
     package = pkgs.cockpit.overrideAttrs (oldAttrs: {
       postInstall = (oldAttrs.postInstall or "") + ''
         ln -s ${cockpit-machines-manual}/share/cockpit/machines $out/share/cockpit/machines
@@ -34,11 +33,13 @@ in
     settings.WebService.Origins = "https://192.168.8.123:9090 http://192.168.8.123:9090";
   };
 
-  # Variable de entorno para asegurar que el bridge mire donde debe
+  # Variable para que Cockpit encuentre los paquetes en el sistema
   systemd.services.cockpit.environment.COCKPIT_DATA_DIR = "/run/current-system/sw/share/cockpit";
 
+  # 2. Virtualización Libvirtd (CORRECCIÓN DE PERSISTENCIA)
   virtualisation.libvirtd = {
     enable = true;
+    onShutdown = "shutdown"; 
     qemu = {
       package = pkgs.qemu_kvm;
       runAsRoot = true;
@@ -47,14 +48,25 @@ in
     };
   };
 
+  # Forzamos que libvirtd NO se desactive por inactividad (evita el cierre de sesión)
+  systemd.services.libvirtd = {
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.ExitType = "cgroup";
+  };
+  systemd.sockets.libvirtd.wantedBy = [ "sockets.target" ];
+
+  # 3. Paquetes del sistema
   environment.systemPackages = with pkgs; [
     cockpit-machines-manual
     virt-manager
+    virt-viewer
     libvirt
+    bridge-utils
   ];
 
+  # 4. Usuario con todos los grupos necesarios para gestionar VMs
   users.users.eracles = {
     isNormalUser = true;
-    extraGroups = [ "libvirtd" "kvm" "wheel" ];
+    extraGroups = [ "libvirtd" "kvm" "wheel" "libvirt" "qemu-libvirtd" ];
   };
 }
